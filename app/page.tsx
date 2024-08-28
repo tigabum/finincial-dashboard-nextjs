03b7e0dfb807a72e29249a5f23edbce025401106
Tigabu <tigabu.m@productera.io>
2024-08-28 11:21:19 +0300

    feat: added Card component

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,11 @@ export default function Page() {
   className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
 />
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to Finance Dashboard.</strong> This is your Dashboard{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+             to manage your finance
             </a>
-            , brought to you by Vercel.
+            , brought to you by Tigabu.
           </p>
           <Link
             href="/login"
