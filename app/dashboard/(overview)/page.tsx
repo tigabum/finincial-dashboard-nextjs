43b4147658c3b0f3d0e457fb98ad84481094f13c
Tigabu <tigabu.m@productera.io>
2024-09-03 23:00:44 +0300

    feat: added grouping components

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,29 +1,24 @@
 import { Suspense } from "react"
 import { fetchRevenue, fetchLatestInvoices, fetchCardData } from "../../lib/data"
-import { Card } from "../../ui/dashboard/cards"
+import CardWrapper, { Card } from "../../ui/dashboard/cards"
 import LatestInvoices from "../../ui/dashboard/latest-invoices"
 import RevenueChart from "../../ui/dashboard/revenue-chart"
 
 import { lusitana } from "../../ui/fonts"
-import { LatestInvoicesSkeleton, RevenueChartSkeleton } from "@/app/ui/skeletons"
+import { CardSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from "@/app/ui/skeletons"
 
 
 
 export default async function page(){
     const revenue = await fetchRevenue()
     const latestInvoices = await fetchLatestInvoices()
-    const {numberOfCustomers,
-        numberOfInvoices,
-        totalPaidInvoices,
-        totalPendingInvoices} = await fetchCardData()
+
    return <main>
     <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`} >Dashboard</h1>
     <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4" >
-        <Card title="Total Paid Invoices" value={totalPaidInvoices} type="collected" />
-        <Card title="Total Pending Invoices" type="pending" value={totalPendingInvoices}/>
-        <Card title="Total Customers" type="customers" value={numberOfCustomers}/>
-        <Card title="Total Invoices" type="invoices" value={numberOfInvoices}/>
-
+        <Suspense fallback={<CardSkeleton/>} >
+            <CardWrapper/>
+        </Suspense>
     </div>
     <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton/>} >
