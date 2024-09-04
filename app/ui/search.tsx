8ed933e2ad523ea41e985caf2e6cb75be2a8c044
Tigabu <tigabu.m@productera.io>
2024-09-04 21:17:15 +0300

    feat: added pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -15,6 +15,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 console.log("search term is", term)
 
     const params = new URLSearchParams(searchParams)
+    params.set('page', '1')
     if (term){
       params.set('query', term)
     }else {
